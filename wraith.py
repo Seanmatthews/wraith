2e715c6df0b44ee07d69571c7a6163681204767a
Sean Matthews <sean.d.matthews@gmail.com>
2018-02-27 09:38:32 -0500

    Establishes local connection to lich

--- wraith.py
+++ wraith.py
@@ -21,6 +21,7 @@ class Wraith:
         self.msg = ''
         self.readbuf = ''
         self.cmds = Queue()
+        self.logfile = open('log.txt', 'w')
         
     @asyncio.coroutine
     def _user_input(self, scr):
@@ -55,52 +56,43 @@ class Wraith:
                     self.msg += chr(last)
 
         except KeyboardInterrupt:
-            return
+            raise
 
     
     @asyncio.coroutine
     def _server_conn(self, scr):
 
-        # With border below, the refresh is writing the border over text at 0,0
-        # because they occupy the same space.
-        #    win_chatline.addstr(5, 5, 'helllooooo\n')
-        #    win_chatbuffer.addstr(5, 5, 'amd me\n')
-        
-        #    win_chatline.border()
-        #    win_chatbuffer.border()
-        #    win_chatline.refresh()
-        #    win_chatbuffer.refresh()
-        
         try:
             loop = asyncio.get_event_loop()
-#            reader, writer = yield from asyncio.open_connection('localhost', 8000, loop=loop)
+            reader, writer = yield from asyncio.open_connection('localhost', 8000, loop=loop)
             
             while True:
                 yield from asyncio.sleep(POLL_INTERVAL, loop=loop)
 
                 # Read incoming
- #               line = reader.readline()
-#                line = ''
-#                if line:
-#                    self.readbuf += line.decode()
-#                else:
-#                    self.readbuf += 'no input this time\n'
-                    
+                try:
+                    linegen = reader.readline()
+                    line = yield from asyncio.wait_for(linegen, timeout=POLL_INTERVAL)
+                    self.readbuf += line.decode()
+                except asyncio.TimeoutError:
+                    pass
+
                 # Write outgoing & add to main window
                 if not self.cmds.empty():
                     cmd = self.cmds.get(block=False)
-#                    writer.write(cmd.encode())
+                    writer.write(cmd.encode())
                     self.readbuf += '> '
-                    self.readbuf += cmd
-#                    self.readbuf += '\n'
+                    self.readbuf += cmd + '\n'
 
         except KeyboardInterrupt:
-            return
+            raise
 
 
     def _redraw_main(self, win):
-        win.addstr(self.readbuf)
-        win.refresh()
+        self.logfile.write(self.readbuf)
+        for line in self.readbuf.splitlines():
+            win.addstr(line + '\n')
+        win.noutrefresh()
 
         
     def _redraw_cmdline(self, win):
@@ -108,11 +100,11 @@ class Wraith:
         win.clear()
         win.border()
         win.addstr(1, 2, self.msg)
-        win.refresh()
+        win.noutrefresh()
 
         
     def _redraw_sidebar(self, win):
-        win.refresh()
+        win.noutrefresh()
 
 
     @asyncio.coroutine
@@ -130,11 +122,8 @@ class Wraith:
         win_main = scr.derwin(*main_hwyx)
 
         # Window properties
-        #m_h, m_w = win_main.getmaxyx()
         win_main.scrollok(True)
         win_main.idlok(True)
-#        win_main.vline(curses.LINES-2, curses.COLS-sidebar_width,
-#                       ACS_VLINE, curses.LINES-2)
         
         try:
             loop = asyncio.get_event_loop()
@@ -158,10 +147,13 @@ class Wraith:
                 s_h, s_w = win_sidebar.getmaxyx()
                 scr.vline(2, sidebar_width, curses.ACS_VLINE, curses.LINES - 5)
                 scr.move(chatline_yx[0] + 1, chatline_yx[1] + len(self.msg) + 2)
-                scr.refresh()
+                scr.noutrefresh()
+
+                # Do update for all noutrefreshed windows here for speed up
+                curses.doupdate()
             
         except KeyboardInterrupt:
-            return
+            raise
 
         
     def main(self, scr):
@@ -169,25 +161,19 @@ class Wraith:
         scr.clear()
         scr.keypad(True)
         scr.nodelay(True)
-        
-        loop = asyncio.get_event_loop()
-        loop.run_until_complete(asyncio.wait([
-            asyncio.async(self._update_windows(scr)),
-            asyncio.async(self._user_input(scr)),
-            asyncio.async(self._server_conn(scr))
-        ]))
-        loop.close()
-    
 
-        # Start threads
-#        executor = concurrent.futures.ThreadPoolExecutor(max_workers=3,)
-#        loop = asyncio.get_event_loop()
-#        try:
-#            loop.run_until_complete(self._server_daemon(executor, scr))
-#        finally:
-#            loop.close()
+        try:
+            loop = asyncio.get_event_loop()
+            loop.run_until_complete(asyncio.wait([
+                asyncio.async(self._update_windows(scr)),
+                asyncio.async(self._user_input(scr)),
+                asyncio.async(self._server_conn(scr))
+            ]))
+            loop.close()
+        except Exception:
+            self.logfile.close()
+            
 
-    
 if __name__ == '__main__':
 
     # Setup logging
